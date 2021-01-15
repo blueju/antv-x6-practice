f8a2cfaae8145b76bd8465794c0f95eacb26356c
blueju <>
2021-01-15 11:43:16 +0800

    update

--- examples/logic-composer/index.jsx
+++ examples/logic-composer/index.jsx
@@ -1,3 +1,9 @@
-export default () => {
-  return <div>逻辑编排</div>;
-};
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+export default class X6Practice extends React.Component {
+  render() {
+    return <Button>逻辑编排</Button>;
+  }
+}
